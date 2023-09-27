e6f21e592dbae5152a59e81d610f00d9ccf0d66e
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-09-27 21:44:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  if (num == 0) {
+    return "0"; 
+  }
+
+  let bin = "";
+  while (num > 0) {
+    let remainder = num % 2; 
+    bin = remainder + bin;
+    num = Math.floor(num / 2);
+  }
+
+  return bin;
 }
 
 window.decimalToBinary = decimalToBinary;
